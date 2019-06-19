8519de7e10c3ff2fde410ad9c7cbdcfef75a7de5
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-06-19 13:47:00 -0400

    Added picture and fluid jumbotron, basic styling on pic, just used a image link for sake of testing, will save image to folder upon completetion

--- backend/client/src/components/HomeJumbo/HomeJumbo.js
+++ backend/client/src/components/HomeJumbo/HomeJumbo.js
@@ -1,8 +1,13 @@
 import React from 'react';
-// import './styles.css';
+import './styles.css';
 
 export const HomeJumbo = () => {
   return (
-   <h1>Test here</h1>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <img id="logoPic" alt="chatter squadron logo" src="https://scontent.fosu1-1.fna.fbcdn.net/v/t1.0-9/60863017_1105744352958124_4856291617184677888_n.jpg?_nc_cat=108&_nc_oc=AQmSXG9LfwNLpRUP3MgyqU7MxsjOgKGdEqjZqPLAbPMIlkc2vc7-XkyTAA2tgWXsXgQ&_nc_ht=scontent.fosu1-1.fna&oh=7d7b8c956a09adc76fc780a1fd1975de&oe=5D87F4CD" />
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
   );
 };
