3070c2605df60bbd78d24b83addef582f61d27f9
giantrotta24 <giantrotta@me.com>
2019-06-22 18:47:48 -0400

    Start More Videos component, add pseudocode

---
+++ src/components/MoreVideos/MoreVideos.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+/*
+Container to hold more videos
+grid
+2x3
+responsive
+video thumbnail
+video title 
+'More Videos' button adds verticle scroll
+with lazy loading
+*/
+
+export default class MoreVideos extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
