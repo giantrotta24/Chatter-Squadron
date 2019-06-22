6fe64fbcdbb77c16dc3ab049afc6458ffc7ddb2f
giantrotta24 <giantrotta@me.com>
2019-06-22 16:53:09 -0400

    merge from Dylan branch adding podcast players

--- src/components/PodcastPlayer/PodcastPlayer.js
+++ src/components/PodcastPlayer/PodcastPlayer.js
@@ -6,6 +6,7 @@ export const PodcastPlayer = (props) => {
     <div className='container my-4'>
       <iframe 
         style={{border: 'none'}} 
+        title="libsyn player"
         src="//html5-player.libsyn.com/embed/episode/id/10159973/height/90/theme/custom/thumbnail/yes/direction/forward/render-playlist/no/custom-color/000000/" 
         height="90" 
         width="100%" 
@@ -16,7 +17,7 @@ export const PodcastPlayer = (props) => {
         oallowfullscreen 
         msallowfullscreen
       />
-      <iframe src="https://open.spotify.com/embed-podcast/episode/65mMmsN1KC9dx8MfJrWG4o" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe src="https://open.spotify.com/embed-podcast/episode/65mMmsN1KC9dx8MfJrWG4o" title="spotify player" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
     );
 };
