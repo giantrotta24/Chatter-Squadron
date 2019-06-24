18d916ff7bba183806c80c4f1d5c5936b86dd66b
Dylan Thomson <djthomson88@gmail.com>
2019-06-24 18:14:26 -0400

    Place podcast player component before videos on smaller screens

--- src/components/PodcastPlayer/PodcastPlayer.js
+++ src/components/PodcastPlayer/PodcastPlayer.js
@@ -17,7 +17,7 @@ export const PodcastPlayer = (props) => {
         oallowfullscreen 
         msallowfullscreen
       />
-      <iframe src="https://open.spotify.com/embed-podcast/episode/65mMmsN1KC9dx8MfJrWG4o" title="spotify player" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      {/* <iframe src="https://open.spotify.com/embed-podcast/episode/65mMmsN1KC9dx8MfJrWG4o" title="spotify player" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
     </div>
     );
 };
