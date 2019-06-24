b229af0bd5dbfb5a2692dc02b672d3f69d49641f
giantrotta24 <giantrotta@me.com>
2019-06-23 23:23:20 -0400

    Small fixes to jumbotron, iframe responsiveness

--- src/components/HomeJumbo/HomeJumbo.js
+++ src/components/HomeJumbo/HomeJumbo.js
@@ -8,7 +8,7 @@ export const HomeJumbo = () => {
     <div className="jumbotron jumbotron-fluid">
     <div className="home-jumbo">
       <img id="logoPic" alt="chatter squadron logo" src={logo} />
-      <iframe id="homeVideo" title="youtube-embed" width="611" height="400" src="https://www.youtube.com/embed/cdcmoyAzY88" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe id="homeVideo" title="youtube-embed" src="https://www.youtube.com/embed/cdcmoyAzY88" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   </div>
   );
