24319402ca5ee583a24fc024b56a3a6fd172fe20
giantrotta24 <giantrotta@me.com>
2019-06-26 12:25:46 -0400

    fixed links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 
 import FacebookLogo from '../images/facebook-brands.svg';
 import TwitterLogo from '../images/twitter-brands.svg';
