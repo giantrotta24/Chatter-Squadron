75240c51eb8c82e2f79a0a4407c3bd90e3ec8941
giantrotta24 <giantrotta@me.com>
2019-07-03 11:04:27 -0400

    Footer icon resize and podcast player 100% width

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,11 +17,11 @@ const Footer = () => {
           {' '}
           © {new Date().getFullYear()}, <a className="footer-link"href="#">Chatter Squadron </a>
         </li>
-        <li className="social"><a className="footer-link"href="https://www.facebook.com/chattersquadron/"><FacebookLogo height="60px"/></a></li>
-        <li className="social"><a className="footer-link"href="https://twitter.com/chattersquadron"><TwitterLogo height="60px"/></a></li>
-        <li className="social"><a className="footer-link"href="https://www.instagram.com/chattersquadron/"><InstagramLogo height="60px"/></a></li>
-        <li className="social"><a className="footer-link"href="https://www.youtube.com/channel/UCq3EOOv6Kk62OyJpjwKzH-g"><YoutubeLogo height="60px"/></a></li>
-        <li className="social"><a className="footer-link"href="#"><PatreonLogo height="60px"/></a></li>
+        <li className="social"><a className="footer-link"href="https://www.facebook.com/chattersquadron/"><FacebookLogo className="social-icons"/></a></li>
+        <li className="social"><a className="footer-link"href="https://twitter.com/chattersquadron"><TwitterLogo className="social-icons"/></a></li>
+        <li className="social"><a className="footer-link"href="https://www.instagram.com/chattersquadron/"><InstagramLogo className="social-icons"/></a></li>
+        <li className="social"><a className="footer-link"href="https://www.youtube.com/channel/UCq3EOOv6Kk62OyJpjwKzH-g"><YoutubeLogo className="social-icons"/></a></li>
+        <li className="social"><a className="footer-link"href="#"><PatreonLogo className="social-icons"/></a></li>
         <li className="db-cp">
           {' '}
           Made by <a className="footer-link"href="#">Dev Boys©</a>
