81aff3bc62e306178157c6ac2b9b0ee10fae8bd6
giantrotta24 <giantrotta@me.com>
2019-07-07 17:28:59 -0400

    pseudo code for controls

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -18,7 +18,7 @@ class VideoPlayer extends Component {
     url: 'https://www.youtube.com/watch?v=PY9XvIA6L6s&t',
     pip: false,
     playing: false,
-    controls: true,
+    controls: false,
     light: true,
     volume: 0.8,
     muted: false,
@@ -27,6 +27,7 @@ class VideoPlayer extends Component {
     duration: 0,
     playbackRate: 1.0,
     loop: false,
+    elWidth: null,
   };
 
   load = url => {
@@ -37,6 +38,39 @@ class VideoPlayer extends Component {
       pip: false,
     });
   };
+
+  /*
+
+  Find video player width and hold value
+  Listen for video player width to change
+  On video width change, check for breakpoint value (max-width: 992px)
+  If <= max-width: 992px then hide styled controls (display: none)
+  Display default youtube controls
+  this.state controls = true.
+
+  componentDidMount = () => {
+    this.setState({
+      elWidth: findDOMNode(this.player).getBoundingClientRect().width,
+    });
+  };
+  determineWidth = () => {
+
+    let elWidth = findDOMNode(this.player).getBoundingClientRect().width
+
+    if (this.state.elWidth && this.state.elWidth !== elWidth) {
+
+      this.setState({
+        elWidth: elWidth
+      })
+
+    }
+
+  };
+  determineControls = () => {
+
+  }
+  */
+
   playPause = () => {
     this.setState({ playing: !this.state.playing });
   };
@@ -115,10 +149,23 @@ class VideoPlayer extends Component {
         <div className="video-controls">
           <button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</button>
           <button onClick={this.onClickFullscreen}>Fullscreen</button>
-          <label htmlFor='volume'>Volume</label>
-          <input id="volume" type='range' min={0} max={1} step='any' value={volume} onChange={this.setVolume} />
-          <label htmlFor='muted'>Muted</label>
-          <input id='muted' type='checkbox' checked={muted} onChange={this.toggleMuted} />
+          <label htmlFor="volume">Volume</label>
+          <input
+            id="volume"
+            type="range"
+            min={0}
+            max={1}
+            step="any"
+            value={volume}
+            onChange={this.setVolume}
+          />
+          <label htmlFor="muted">Muted</label>
+          <input
+            id="muted"
+            type="checkbox"
+            checked={muted}
+            onChange={this.toggleMuted}
+          />
         </div>
       </div>
     );
