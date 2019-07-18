91dc6b157508afb9303fb76ab21100fd7e2d083b
giantrotta24 <giantrotta@me.com>
2019-07-18 14:08:24 -0400

    window variables fix

--- src/components/MoreVideos.js
+++ src/components/MoreVideos.js
@@ -4,7 +4,7 @@ import debounce from 'lodash.debounce';
 import styled from 'styled-components';
 import axios from 'axios';
 const windowInnerHeight = typeof window !== 'undefined' && window.innerHeight;
-const window = typeof window !== 'undefined' && window.onscroll;
+const window = window !== 'undefined' && window.onscroll;
 const documentScrollTop =
   typeof document !== 'undefined' && document.documentElement.scrollTop;
 const documentOffsetHeight =
