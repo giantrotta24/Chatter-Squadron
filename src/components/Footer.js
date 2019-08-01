0d8e603cd2e22998a2076709f4611de8bd193e97
Dylan Thomson <djthomson88@gmail.com>
2019-08-01 13:20:54 -0400

    Fix href warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import FacebookLogo from '../assets/svg/facebook-brands.svg';
 import TwitterLogo from '../assets/svg/twitter-brands.svg';
@@ -15,17 +16,18 @@ const Footer = () => {
       <ul>
         <li className='cs-cp'>
           {' '}
-          © {new Date().getFullYear()}, <a className='footer-link'href='#'>Chatter Squadron </a>
+          © {new Date().getFullYear()}, <Link to='/index' className='footer-link'>Chatter Squadron </Link>
         </li>
-        <li className='social'><a className='footer-link'href='https://www.facebook.com/chattersquadron/'><FacebookLogo className='social-icons'/></a></li>
-        <li className='social'><a className='footer-link'href='https://twitter.com/chattersquadron'><TwitterLogo className='social-icons'/></a></li>
-        <li className='social'><a className='footer-link'href='https://www.instagram.com/chattersquadron/'><InstagramLogo className='social-icons'/></a></li>
-        <li className='social'><a className='footer-link'href='https://www.youtube.com/channel/UCq3EOOv6Kk62OyJpjwKzH-g'><YoutubeLogo className='social-icons'/></a></li>
-        <li className='social'><a className='footer-link'href='https://www.patreon.com/ChatterSquadron'><PatreonLogo className='social-icons'/></a></li>
-        <li className='social'><a className='footer-link'href='http://chattersquadron.libsyn.com/rss'><RssLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='https://www.facebook.com/chattersquadron/'><FacebookLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='https://twitter.com/chattersquadron'><TwitterLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='https://www.instagram.com/chattersquadron/'><InstagramLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='https://www.youtube.com/channel/UCq3EOOv6Kk62OyJpjwKzH-g'><YoutubeLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='https://www.patreon.com/ChatterSquadron'><PatreonLogo className='social-icons'/></a></li>
+        <li className='social'><a className='footer-link' href='http://chattersquadron.libsyn.com/rss'><RssLogo className='social-icons'/></a></li>
         <li className='db-cp'>
           {' '}
-          Made by <a className='footer-link'href='#'>GTSS ©</a>
+          {/* The link below has no place to go yet. We'll have to update when we make our portfolio site */}
+          Made by <div className='footer-link'>GTSS ©</div>
         </li>
       </ul>
     </footer>
